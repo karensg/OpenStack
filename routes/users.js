3e615a1f8d8988186d31579c3ba99838184be062
Karens <yetti4@gmail.com>
2014-10-07 16:13:04 +0300

    Installed ExpressJS and linked to MongoDB

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  var db = req.db;
+    db.collection('usercollection').find().toArray(function (err, items) {
+        res.json(items);
+    });
+});
+
+module.exports = router;
