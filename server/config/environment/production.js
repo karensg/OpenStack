943afc5271d73849bb42208d4118ced8e94ce6c1
Karens <yetti4@gmail.com>
2014-10-30 21:05:38 +0200

    Deleted IP form config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -3,15 +3,9 @@
 // Production specific configuration
 // =================================
 module.exports = {
-  // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            '130.233.42.191',
-
-  // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            '8080',
+            8080,
 
   // MongoDB connection options
   mongo: {
